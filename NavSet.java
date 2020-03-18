5e577442baf0319643b4fce2449cf15d4767ae47
Niaz <yazzzik@list.ru>
2020-03-18 22:03:01 +0300

    Added javadoc

--- NavSet.java
+++ NavSet.java
@@ -2,6 +2,15 @@ package inf5;
 
 import java.util.*;
 
+/**
+* Aminov Niaz, 11-903. First course, second semester. 
+* Student of Kazan Federal University, faculty of the Higher School of Information Technology and Intelligent Systems.
+* 
+* This class implements the NavigableSet interface and is designed to be able to find the closest matches by a given value.
+* 
+* 11.03.2020
+*/
+
 public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 	private T[] data;
 	
@@ -12,6 +21,11 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		this.comp = comparator;
 	}
 	
+	/**
+	 * The NavSet class constructor assigns an array and a predefined comparator.
+	 * Elements of array must be sorted initially.
+	*/
+	
 	public NavSet(T[] arr, Comparator<T> comparator) {
 		this.data = arr;
 		this.comp = comparator;
@@ -21,58 +35,97 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 	public Iterator<T> iterator() {
 		return new ArrIterator(data);
 	}
+	
+	/**
+	 * Returns the length of the list.
+	 */
 
 	@Override
 	public int size() {
 		return data.length;
 	}
 	
+	/**
+	 * Adds an element to the set; if there is no such element, it returns true, otherwise it returns false.
+	 */
+	
 	public boolean add(T obj) {
-		T[] newArr = (T[]) new Object[data.length + 1];
-		int j = 0;
-		for (int i = 0; i < newArr.length; i++) {
-			if (i == data.length & i == j) {
-				newArr[i] = obj;
-				break;
+		if (!has(obj)) {
+			T[] newArr = (T[]) new Object[data.length + 1];
+			int j = 0;
+			for (int i = 0; i < newArr.length; i++) {
+				if (i == data.length & i == j) {
+					newArr[i] = obj;
+					break;
+				}
+				if (data[i].equals(obj) & comp.compare(data[i], obj) == 0) { 
+					return false;
+				} else if(comp.compare(data[i], obj) < 0){
+					newArr[j] = obj;
+					j++;
+				} else {
+					newArr[i] = data[j];
+					j++;
+				}
 			}
-			if (data[i].equals(obj) & comp.compare(data[i], obj) == 0) { 
-				return false;
-			} else if(comp.compare(data[i], obj) < 0){
-				newArr[j] = obj;
-				j++;
-			} else {
-				newArr[i] = data[j];
-				j++;
-			}
+			this.data = newArr;
+			return true;
 		}
-		this.data = newArr;
-		return true;
+		return false;
 	}
 	
+	/**
+	 * Checks if the set contains a given object.
+	 */
+	
+	public boolean has(T obj) {
+		for (int i = 0; i < data.length; i++) {
+			if (data[i].equals(obj)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	@Override
 	public Comparator<? super T> comparator() {
 		return comp;
 	}
+	
+	/**
+	 * Returns the first element of a set
+	 */
 
 	@Override
 	public T first() {
 		return data[0];
 	}
+	
+	/**
+	 * Returns the last element of a set
+	 */
 
 	@Override
 	public T last() {
 		return data[data.length - 1];
 	}
+	
+	/**
+	 * Returns the next element of the set.
+	 */
 
 	@Override
 	public T ceiling(T obj) {
 		int index = indexOf(obj);
-		if (index < data.length - 1) {
+		if (index < data.length - 1 & index != -1) {
 			return data[index + 1];
 		}
 		return null;
 	}
+	
+	/**
+	 * Returns an iterator for a set with values in reverse order.
+	 */
 
 	@Override
 	public Iterator<T> descendingIterator() {
@@ -83,6 +136,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return new ArrIterator(newData);
 	}
+	
+	/**
+	 * Returns the original set with values in reverse order
+	 */
 
 	@Override
 	public NavigableSet<T> descendingSet() {
@@ -93,6 +150,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return new NavSet<T>(newData, comp);
 	}
+	
+	/**
+	 * Returns the previous element of the set.
+	 */
 
 	@Override
 	public T floor(T obj) {
@@ -102,6 +163,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return null;
 	}
+	
+	/**
+	 * Returns a SortedSet that begins with this element
+	 */
 
 	@Override
 	public SortedSet<T> headSet(T obj) {
@@ -112,6 +177,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return new NavSet<T>(newData, comp);
 	}
+	
+	/**
+	 * Returns a Navigable that begins with this element. If the 'inclusive' is true - including, if false - exclusively.
+	 */
 
 	@Override
 	public NavigableSet<T> headSet(T obj, boolean inclusive) {
@@ -125,6 +194,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return new NavSet<T>(newData, comp);
 	}
+	
+	/**
+	 * Returns the maximum element of the set after the given object.
+	 */
 
 	@Override
 	public T higher(T obj) {
@@ -138,6 +211,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return null;
 	}
+	
+	/**
+	 * Returns the minimum element of the set before this object.
+	 */
 
 	@Override
 	public T lower(T obj) {
@@ -151,6 +228,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return null;
 	}
+	
+	/**
+	 * Removes the first element of the set and returns it.
+	 */
 
 	@Override
 	public T pollFirst() {
@@ -167,6 +248,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		data = newData;
 		return obj;
 	}
+	
+	/**
+	 * Removes the last element of the set and returns it.
+	 */
 
 	@Override
 	public T pollLast() {
@@ -181,6 +266,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		data = newData;
 		return obj;
 	}
+	
+	/**
+	 * Returns a SortedSet whose elements are in the specified range.
+	 */
 
 	@Override
 	public SortedSet<T> subSet(T fromEl, T toEl) {
@@ -196,6 +285,11 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return new NavSet<T>(newData, comp);
 	}
+	
+	/**
+	 * Returns a NavigableSet whose elements are in the specified range. 
+	 * Inclusive, depending on the values of lowerInclusive and higherInclusive
+	 */
 
 	@Override
 	public NavigableSet<T> subSet(T fromEl, boolean lowerInclusive, T toEl, boolean higherInclusive) {
@@ -219,6 +313,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return new NavSet<T>(newData, comp);
 	}
+	
+	/**
+	 * Returns a set whose elements are larger than the passed object.
+	 */
 
 	@Override
 	public SortedSet<T> tailSet(T obj) {
@@ -233,6 +331,11 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return new NavSet<T>(newData, comp);
 	}
+	
+	/**
+	 * Returns a set whose elements are larger than the passed object. 
+	 * The element is inclusive depending on the value of the 'inclusive'.
+	 */
 
 	@Override
 	public NavigableSet<T> tailSet(T obj, boolean inclusive) {
@@ -250,6 +353,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 		}
 		return new NavSet<T>(newData, comp);
 	}
+	
+	/**
+	 * Returns the index of this element in the set.
+	 */
 	
 	public int indexOf(T obj) {
 		for (int i = 0; i < data.length; i++) {
@@ -285,6 +392,10 @@ public class NavSet<T> extends AbstractSet<T> implements NavigableSet<T>{
 			return obj;
 		}
 	}
+	
+	/**
+	 * testing...
+	 */
 	
 	public static void main(String[] args) {
 		Book book1 = new Book("Lev Tolstoy", "War and Peace");
